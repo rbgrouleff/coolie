01e1d61b2535c471bbf123ebbc11d3ef6d200d23
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2013-04-08 17:39:28 +0200

    Moved a couple of tests around

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -13,25 +13,15 @@ class Worker
 end
 
 describe Worker do
-
-  it 'executes the passed on block' do
-    tmpfile = Tempfile.new 'foo'
-    worker = Worker.new do
-      tmpfile.write 'foo'
-    end
-    worker.start!
-    Process.wait worker.pid
-    tmpfile.rewind
-    tmpfile.read.should eq('foo')
-    tmpfile.close
-    tmpfile.unlink
-  end
-
   subject { Worker.new {} }
 
   context 'that has not been started yet' do
     it 'should not have a pid' do
       subject.pid.should be_nil
+    end
+
+    it 'should have a ppid' do
+      subject.ppid.should_not be_nil
     end
   end
 
@@ -39,7 +29,20 @@ describe Worker do
     before { subject.start! }
 
     it 'should have a pid' do
-      subject.pid.should_not be_nil
+      subject.pid.should_not eq(subject.ppid)
+    end
+
+    it 'executes the passed on block' do
+      tmpfile = Tempfile.new 'foo'
+      worker = Worker.new do
+        tmpfile.write "PPID is: #{Process.ppid}"
+      end
+      worker.start!
+      Process.wait worker.pid
+      tmpfile.rewind
+      tmpfile.read.should eq("PPID is: #{worker.ppid}")
+      tmpfile.close
+      tmpfile.unlink
     end
   end
 end
