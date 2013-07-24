d119f1f84ffabe690ee0c2f90b69642e28db046c
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2013-07-24 15:54:13 +0200

    Implemented a very simple master

--- lib/coolie/master.rb
+++ lib/coolie/master.rb
@@ -1,4 +1,37 @@
+require_relative './worker'
+
 module Coolie
   class Master
+    def initialize(job)
+      @child_pids = []
+      @job = job
+    end
+
+    def start_worker
+      if child = fork
+        @child_pids << child
+      else
+        worker = Worker.new(@job)
+        Signal.trap('INT') do
+          worker.stop
+        end
+        worker.start
+        exit!
+      end
+    end
+
+    def stop_worker
+      if child = @child_pids.first
+        Process.kill "INT", child
+        Process.waitpid2 child
+        @child_pids.delete child
+      else
+        raise 'No workers running'
+      end
+    end
+
+    def child_count
+      @child_pids.length
+    end
   end
 end
--- spec/coolie/master_spec.rb
+++ spec/coolie/master_spec.rb
@@ -2,7 +2,67 @@ require_relative '../../lib/coolie/master'
 
 module Coolie
   describe Master do
-    it 'spawns a worker' do
+    subject(:master) { Master.new nil }
+
+    describe 'when receiving the start_worker message' do
+      it 'forks' do
+        master.should_receive(:fork) { 666 }
+        master.start_worker
+      end
+
+      it 'starts a worker after forking' do
+        master.should_receive(:fork) { nil }
+        master.stub :exit!
+
+        worker = double :worker
+        worker.should_receive :start
+        Worker.should_receive(:new) { worker }
+
+        master.start_worker
+      end
+
+      it 'increases child_count' do
+        master.stub(:fork) { 666 }
+        master.start_worker
+        master.child_count.should eq(1)
+      end
+    end
+
+    describe 'when receiving the stop_worker message' do
+      describe 'and it has running workers' do
+        before :each do
+          master.stub(:fork) { 666 }
+          master.start_worker
+        end
+
+        it 'kills a child with the INT signal' do
+          Process.should_receive(:kill).with('INT', 666)
+          Process.stub(:waitpid2).with(666)
+
+          master.stop_worker
+        end
+
+        it 'waits for the killed child to stop' do
+          Process.stub(:kill).with('INT', 666)
+          Process.should_receive(:waitpid2).with(666)
+
+          master.stop_worker
+        end
+
+        it 'decreases child_count' do
+          Process.stub(:kill).with('INT', 666)
+          Process.stub(:waitpid2).with(666)
+
+          master.stop_worker
+          master.child_count.should eq(0)
+        end
+      end
+
+      describe 'and there are no running workers' do
+        it 'raises an error' do
+          expect { master.stop_worker }.to raise_error('No workers running')
+        end
+      end
     end
   end
 end
