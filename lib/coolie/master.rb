5df043e9d3bfff93f844cb4123e53b5426cc3e16
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2013-09-03 10:45:24 +0200

    Master is giving pipe to worker and monitors the running workers

--- lib/coolie/master.rb
+++ lib/coolie/master.rb
@@ -2,17 +2,31 @@ require_relative './worker'
 
 module Coolie
   class Master
-    def initialize(job)
+    def initialize(job, options = {})
+      @number_of_workers = options.fetch :workers, 0
       @child_pids = []
       @job = job
     end
 
+    def start
+      if @number_of_workers > 0
+        trap_signals
+        @number_of_workers.times do
+          start_worker
+        end
+        monitor_workers
+      end
+    end
+
     def start_worker
+      reader, writer = IO.pipe
       if child = fork
+        writer.close
         @child_pids << child
       else
-        worker = Worker.new(@job)
-        process_name = "Worker #{Process.pid}"
+        reader.close
+        worker = Worker.new(@job, writer)
+        self.process_name = "Worker #{Process.pid}"
         Signal.trap('INT') do
           worker.stop
         end
@@ -21,19 +35,19 @@ module Coolie
       end
     end
 
-    def stop_worker
-      if child = @child_pids.first
-        Process.kill "INT", child
-        Process.waitpid2 child
-        @child_pids.delete child
+    def stop_worker(wpid)
+      if @child_pids.include?(wpid)
+        Process.kill "INT", wpid
+        Process.waitpid2 wpid
+        @child_pids.delete wpid
       else
-        raise 'No workers running'
+        raise "Unknown worker PID: #{wpid}"
       end
     end
 
     def stop_all
-      while child_count > 0 do
-        stop_worker
+      @child_pids.each do |wpid|
+        stop_worker(wpid)
       end
     end
 
@@ -41,7 +55,38 @@ module Coolie
       @child_pids.length
     end
 
+    def monitor_workers
+      loop do
+        if workers = crashed_workers
+          restart_workers workers
+        end
+      end
+    end
+
     private
+
+    def crashed_workers
+      readers = IO.select(@workers.map { |w| w[:reader] }, nil, nil, 1)
+      if readers
+        readers.map { |reader| worker_pid(reader) }
+      else
+        nil
+      end
+    end
+
+    def restart_workers(workers)
+      workers.each do |wpid|
+        stop_worker wpid
+        start_worker
+      end
+    end
+
+    def trap_signals
+      Signal.trap 'INT' do
+        stop_all
+        exit 0
+      end
+    end
 
     def process_name=(name)
       $0 = name
--- spec/coolie/master_spec.rb
+++ spec/coolie/master_spec.rb
@@ -2,29 +2,72 @@ require_relative '../../lib/coolie/master'
 
 module Coolie
   describe Master do
-    subject(:master) { Master.new nil }
+    let(:job) { double(:job) }
+    subject(:master) { Master.new job }
 
     describe 'when receiving the start_worker message' do
+      let(:pipes) { [double(:reader_pipe), double(:writer_pipe)] }
+
       it 'forks' do
         master.should_receive(:fork) { 666 }
         master.start_worker
       end
 
-      it 'starts a worker after forking' do
-        master.should_receive(:fork) { nil }
-        master.stub :exit!
+      describe 'in the worker process' do
+        let(:worker) { double :worker }
 
-        worker = double :worker
-        worker.should_receive :start
-        Worker.should_receive(:new) { worker }
+        before :each do
+          master.stub(:fork) { nil }
+          IO.stub(:pipe) { pipes }
+          pipes.first.stub(:close)
+          Process.stub(:pid) { 666 }
+          master.stub :exit!
+          Worker.stub(:new) { worker }
+          worker.stub(:start)
+        end
 
-        master.start_worker
+        it 'should rename the process' do
+          master.should_receive(:process_name=).with("Worker #{666}")
+          master.start_worker
+        end
+
+        it 'starts a worker after forking' do
+          worker.should_receive :start
+          master.start_worker
+        end
+
+        it 'gives the writer pipe to the worker' do
+          Worker.should_receive(:new).with(job, pipes.last) { worker }
+          master.start_worker
+        end
+
+        it 'closes the reader pipe' do
+          pipes.first.should_receive :close
+          master.start_worker
+        end
       end
 
-      it 'increases child_count' do
-        master.stub(:fork) { 666 }
-        master.start_worker
-        master.child_count.should eq(1)
+      describe 'in the master process' do
+        before :each do
+          master.stub(:fork) { 666 }
+        end
+
+        it 'increases child_count' do
+          master.start_worker
+          master.child_count.should eq(1)
+        end
+
+        it 'should open a pipe' do
+          IO.should_receive(:pipe) { pipes }
+          pipes.last.stub(:close)
+          master.start_worker
+        end
+
+        it 'should close the writer pipe' do
+          IO.stub(:pipe) { pipes }
+          pipes.last.should_receive :close
+          master.start_worker
+        end
       end
     end
 
@@ -39,21 +82,21 @@ module Coolie
           Process.should_receive(:kill).with('INT', 666)
           Process.stub(:waitpid2).with(666)
 
-          master.stop_worker
+          master.stop_worker(666)
         end
 
         it 'waits for the killed child to stop' do
           Process.stub(:kill).with('INT', 666)
           Process.should_receive(:waitpid2).with(666)
 
-          master.stop_worker
+          master.stop_worker(666)
         end
 
         it 'decreases child_count' do
           Process.stub(:kill).with('INT', 666)
           Process.stub(:waitpid2).with(666)
 
-          master.stop_worker
+          master.stop_worker(666)
           master.child_count.should eq(0)
         end
       end
@@ -62,7 +105,7 @@ module Coolie
         Process.stub(:kill).with('INT', 666)
         Process.stub(:waitpid2).with(666)
 
-        master.should_receive(:stop_worker).exactly(master.child_count).times.and_call_original
+        master.should_receive(:stop_worker).with(666).exactly(master.child_count).times.and_call_original
 
         master.stop_all
       end
@@ -71,7 +114,7 @@ module Coolie
     describe 'when there are no running workers' do
       describe 'and it receives stop_worker' do
         it 'raises an error' do
-          expect { master.stop_worker }.to raise_error('No workers running')
+          expect { master.stop_worker(666) }.to raise_error("Unknown worker PID: #{666}")
         end
       end
 
@@ -80,6 +123,49 @@ module Coolie
           master.should_not_receive(:stop_worker)
           master.stop_all
         end
+      end
+    end
+
+    it 'starts the specified number of workers when started' do
+      master = Master.new nil, workers: 3
+      master.stub :monitor_workers
+      master.should_receive(:start_worker).exactly(3).times
+      master.start
+    end
+
+    describe 'when number of workers is zero' do
+      let(:master) { Master.new nil, workers: 0 }
+
+      it 'should not attach signal handler' do
+        Signal.should_not_receive(:trap)
+        master.start
+      end
+
+      it 'should not start workers' do
+        master.should_not_receive :start_worker
+        master.start
+      end
+
+      it 'should not monitor workers' do
+        master.should_not_receive(:monitor_workers)
+        master.start
+      end
+    end
+
+    describe 'when number of workers is non-zero' do
+      let(:master) { Master.new nil, workers: 2 }
+
+      it 'attaches a signal handler when started' do
+        Signal.should_receive(:trap).with('INT')
+        master.stub :start_worker
+        master.stub :monitor_workers
+        master.start
+      end
+
+      it 'should monitor the workers' do
+        master.stub :start_worker
+        master.should_receive :monitor_workers
+        master.start
       end
     end
   end
