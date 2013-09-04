af340ea0f13cb952b8061b74cfc39b426dc1ed4c
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2013-09-04 11:35:27 +0200

    Making the sleep job a bit talkative and unstable..

--- lib/coolie/sleep.rb
+++ lib/coolie/sleep.rb
@@ -3,7 +3,9 @@ require_relative './job'
 module Coolie
   class Sleep < Job
     def perform
-      sleep 20
+      sleep 2
+      puts "Goodmorning from #{Process.pid}"
+      raise "Hest" if rand(10) % 2 == 0
     end
   end
 end
