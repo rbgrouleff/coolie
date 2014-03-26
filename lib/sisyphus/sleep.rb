26c1058708e5081e6088b113aca86c2dfbff55b6
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2014-03-26 13:06:01 +0100

    Longer sleep period

--- lib/sisyphus/sleep.rb
+++ lib/sisyphus/sleep.rb
@@ -3,7 +3,7 @@ require_relative './job'
 module Sisyphus
   class Sleep < Job
     def perform
-      sleep 2
+      sleep 40
       puts "Goodmorning from #{Process.pid}"
       raise "Hest" if rand(10) % 2 == 0
     end
